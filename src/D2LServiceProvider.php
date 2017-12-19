a0f2cddf8dbe015c70d85d9b297df9ec2c444ed7
Oscar Chang <oscarchang1226@gmail.com>
2017-12-19 09:58:55 -0600

    init for laravel package

---
+++ src/D2LServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Desire2Learn\Valence;
+
+use Illuminate\Support\ServiceProvider;
+
+class D2LServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
